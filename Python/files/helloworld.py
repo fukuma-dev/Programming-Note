3e0f663e7c525e901dcf0ef9da474ffa00617e0c
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2019-10-22 13:27:44 +0900

    python基本構文

---
+++ Python/files/helloworld.py
@@ -0,0 +1,52 @@
+# coding: utf-8 python3では必要なし
+
+# Comment
+
+"""
+Comment
+"""
+
+print("Hello World")
+
+msg = "Hello Message"
+
+print(msg)
+
+# 定数: 再代入できない
+ADMIN_EMAIL = "sample@gmail.com"
+
+# 文字列
+s = "hel\nlo wor\tld"
+html = """<html>
+<body></body>
+</html>"""
+
+print(s)
+print(html)
+
+# 整数
+i = 10
+f = 23.4
+
+# 論理値
+flag = True # False
+
+# + - * / // % **
+x = 10
+print(x / 3) # 3.33...
+print(x // 3) # 3
+print(x % 3) # 1
+print(x ** 2) # 100
+
+y = 4
+y += 12
+print(y) # 16
+
+# and or not
+print(True and False) # False
+print(True or False) # True
+print(not True) # False
+
+# + *
+print("Hello" + "World")
+print("Hello" * 3)
